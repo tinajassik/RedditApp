7adabc3e81fc9a598c2fda51ff872e836eaee8bc
Kristina Jassova <kristinka.jassova@gmail.com>
2023-04-12 10:52:46 +0200

    final

--- Domain/Auth/AuthorizationPolicies.cs
+++ Domain/Auth/AuthorizationPolicies.cs
@@ -12,20 +12,6 @@ public class AuthorizationPolicies
         {
             options.AddPolicy("MustBeLoggedIn", a =>
                 a.RequireAuthenticatedUser());
-    
-            options.AddPolicy("SecurityLevel4", a =>
-                a.RequireAuthenticatedUser().RequireClaim("SecurityLevel", "4", "5"));
-    
-            options.AddPolicy("MustBeTeacher", a =>
-                a.RequireAuthenticatedUser().RequireClaim("Role", "Teacher"));
-    
-            options.AddPolicy("SecurityLevel2OrAbove", a =>
-                a.RequireAuthenticatedUser().RequireAssertion(context =>
-                {
-                    Claim? levelClaim = context.User.FindFirst(claim => claim.Type.Equals("SecurityLevel"));
-                    if (levelClaim == null) return false;
-                    return int.Parse(levelClaim.Value) >= 2;
-                }));
         });
     }
     
